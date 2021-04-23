cb0b63b5395da2ab733bcb1b8a1bd1c0ed29c9a2
manafhijazi <qumsanmanaf@gmail.com>
2021-04-24 02:22:02 +0300

    add button for smaller screens

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -34,6 +34,11 @@ export const HomeLayout = () => {
           }}>
           <img src={language === 'ar' ? usa : uae} alt='language' width='24px' />
         </IconButton>
+        <div className='view-content-icon'>
+          <IconButton onClick={() => window.open('https://cors-anywhere.herokuapp.com/corsdemo')}>
+            <span className='iconify' data-icon='mdi-eye' />
+          </IconButton>
+        </div>
         <div className='view-content-button'>
           <Button onClick={() => window.open('https://cors-anywhere.herokuapp.com/corsdemo')}>
             Click me to view content
