f8ea60b0bd519cdf677797e9040a631c6fd01a2d
manafhijazi <qumsanmanaf@gmail.com>
2021-04-24 00:02:13 +0300

    Add proxy for cors problem

--- src/Services/JobsServices.jsx
+++ src/Services/JobsServices.jsx
@@ -1,6 +1,7 @@
 import { HttpServices, showError } from '../Helper';
 
-const baseUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/';
+// I'm using https://thingproxy.freeboard.io/fetch/ proxy to bypass the CORS error
+const baseUrl = 'https://thingproxy.freeboard.io/fetch/https://jobs.github.com/';
 export const GetAllJobOpenings = async ({ description, location }, page) => {
   const queryList = [];
   if (description) queryList.push(`description=${description}`);
@@ -8,13 +9,13 @@ export const GetAllJobOpenings = async ({ description, location }, page) => {
   if (page) queryList.push(`page=${page}`);
   const result = await HttpServices.get(`${baseUrl}positions.json?${queryList.join('&')}`)
     .then((data) => data)
-    .catch((error) => showError(error.statusText));
+    .catch((error) => showError(error));
   return result;
 };
 
 export const GetAllJobOpeningsById = async (jobId) => {
   const result = await HttpServices.get(`${baseUrl}positions/${jobId}.json`)
     .then((data) => data)
-    .catch((error) => showError(error.statusText));
+    .catch((error) => showError(error));
   return result;
 };
